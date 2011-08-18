acac6e5c0bc411c97b1ca02adf17d1b555d0a344
gabo0607@gmail.com <gabo0607@gmail.com@7116a9e5-e746-3519-f151-562531c1d9b1>
2011-08-18 14:02:15 +0000

    json

---
+++ trunk/sideccop/jsonp/grid-forestales.php
@@ -0,0 +1,27 @@
+{
+   "total":"4",
+   "data":[
+      {
+         "co_renglon":"1",
+         "nb_forestal":"carne",
+         "va_precio_venta":"200"
+		},
+      {
+         "co_renglon":"2",
+         "nb_forestal":"leche",
+         "va_precio_venta":"100"
+      },
+      {
+         "co_renglon":"3",
+         "nb_forestal":"arina",
+         "va_precio_venta":"350"
+
+      },
+      {
+         "co_renglon":"4",
+         "nb_forestal":"pan",
+         "va_precio_venta":"450"
+
+      }
+   ]
+}
