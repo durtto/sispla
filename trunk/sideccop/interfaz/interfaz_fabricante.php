ac64b0c7b359be7d5e0001ed5bab77ec50d57249
geryisjfv <geryisjfv@7116a9e5-e746-3519-f151-562531c1d9b1>
2011-08-31 14:58:48 +0000

    Fabricante listo y funcionando

--- trunk/sideccop/interfaz/interfaz_fabricante.php
+++ trunk/sideccop/interfaz/interfaz_fabricante.php
@@ -72,5 +72,5 @@
 		
 	}
 		
-		echo $resultado2='{"total":'.$total.', "fabricante":'.json_encode($resultado).'}';
+		echo $resultado2='{"total":'.$total.', "fabricantes":'.json_encode($resultado).'}';
 ?>
