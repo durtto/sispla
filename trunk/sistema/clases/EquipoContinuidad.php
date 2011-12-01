8246333b126178e0c8cf36e9b9b70d88ffbca7c8
gabo0607@gmail.com <gabo0607@gmail.com@7116a9e5-e746-3519-f151-562531c1d9b1>
2011-12-01 17:42:29 +0000

    Equipo Continuidad listo

--- trunk/sistema/clases/EquipoContinuidad.php
+++ trunk/sistema/clases/EquipoContinuidad.php
@@ -64,7 +64,7 @@ class EquipoContinuidad extends MyPDO
   public function actualizarEquipoContinuidad($equipocont, $condiciones) {
   	$this->pdo->beginTransaction();	
 
-	$equipocont = array_intersect_key($cliente, $this->columEquipoContinuidad);
+	$equipocont = array_intersect_key($equipocont, $this->columEquipoContinuidad);
 	
 	$r1 = $this->pdo->_update('tr061_equipo_continuidad', $equipocont, $condiciones);
 	
@@ -80,7 +80,7 @@ class EquipoContinuidad extends MyPDO
    * @return string
    * @access public
    */
-  public function eliminarEquipoContinuidad($condiciones ) {
+  public function eliminarEquipoContinuidad($condiciones) {
   
   	$this->pdo->beginTransaction();	
 
@@ -98,15 +98,17 @@ class EquipoContinuidad extends MyPDO
     public function cargarEquipoContinuidad($start='0', $limit='ALL', $sort = "", $dir = "ASC") {
 
 	$query = "SELECT 
+		  e.co_equipo_continuidad,
+		  p.co_indicador,
+		  p.nu_cedula,
 		  p.nb_persona, 
 		  p.tx_apellido, 
-		  p.di_oficina, 
 		  r.co_rol_resp,
 		  r.nb_rol_resp 
-			FROM 
+		 FROM 
 		 tr061_equipo_continuidad e 
-		 INNER JOIN tr002_rol_responsabilidad r ON (e.co_rol_resp = r.co_rol_resp)
-		 LEFT JOIN tr010_persona p ON (e.co_indicador = p.co_indicador)";
+		 INNER JOIN tr010_persona p ON (e.co_indicador = p.co_indicador)
+		 LEFT JOIN tr002_rol_responsabilidad r ON (e.co_rol_resp = r.co_rol_resp)";
 	if ($sort != "") {
 	$query .= " ORDER BY ".$sort." ".$dir;
 	}
